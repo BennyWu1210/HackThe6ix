414d6044cd52fa143b2872043f96f32901238946
bennywu2019 <benny.wu.new@gmail.com>
2022-08-20 11:25:09 -0400

    home page hero section

--- website/src/pages/Home.jsx
+++ website/src/pages/Home.jsx
@@ -13,9 +13,25 @@ function Home() {
         <div className="home-content">
           <div className="home-title">Find Your Perfect Match</div>
           <div className="home-content">
-            Dog Tinder is a website for fellow pet owners to find each other! 💛
+            Dog Tinder is a website for fellow pet owners to find their missing
+            pets from the generosity of other friends 💛
           </div>
         </div>
+      </div>
+
+      <div className="home-page">
+        <div className="page-heading">
+          <div className="page-circles">
+            <span className="dot" id="dot1" />
+            <span className="dot" id="dot2" />
+            <span className="dot" id="dot3" />
+            <span className="dot" id="dot4" />
+          </div>
+          <div className="page-title">
+            Find Your Lost Pets with a 69.69% Success Rate
+          </div>
+        </div>
+        <div className="page-cards"></div>
       </div>
     </div>
   );
