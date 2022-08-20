db4098db777a11d17f9448d13929393321074261
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2022-08-20 14:14:26 -0400

    swiper no swiping

--- website/src/pages/Swipe.jsx
+++ website/src/pages/Swipe.jsx
@@ -3,6 +3,25 @@ import Navbar from "../components/Navbar";
 import "./Swipe.css"
 
 export default function Swipe() {
+    let touchStart = 0
+    let touchEnd = 0
+        
+    function checkDirection() {
+        if (touchEnd < touchStart) {
+            console.log("left")
+        } else if (touchEnd > touchStart) {
+            console.log("right")
+        }
+    }
+
+    document.addEventListener('touchstart', e => {
+        touchStart = e.changedTouches[0].screenX
+    })
+
+    document.addEventListener('touchend', e => {
+        touchEnd = e.changedTouches[0].screenX
+    checkDirection()
+    })
     return (
         <div>
             <Navbar color="red"></Navbar>
