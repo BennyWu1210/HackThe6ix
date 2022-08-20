7d57ab32e2ba882fca4ad471d3c5b8db4fdf0f39
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2022-08-19 20:55:20 -0400

    magic happened

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="website/build/static", template_folder="website/build")
 
 @app.route("/")
 def index() -> str:
-    return render_template("index.html")
+    return render_template('index.html')
