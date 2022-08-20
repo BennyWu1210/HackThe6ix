16ddc2e0f7ad4a179d93a16cf9ba37844fc6aa26
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2022-08-20 14:44:41 -0400

    added sizes and contact options

--- pet_finder/parser.py
+++ pet_finder/parser.py
@@ -3,3 +3,9 @@ import json
 
 def pet_info() -> str:
     return json.dumps(options.ANIMAL)
+
+def get_sizes() -> list:
+    sizes = []
+    for size in options.SIZE:
+        sizes.append(f"{size}.jpg")
+    return str(sizes)
