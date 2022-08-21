a1a4c008afce7edfe44c3e9c19a9701ed92ea3a6
bennywu2019 <benny.wu.new@gmail.com>
2022-08-21 08:24:01 -0400

    bruh

--- website/src/pages/Swipe.jsx
+++ website/src/pages/Swipe.jsx
@@ -18,30 +18,16 @@ export default function Swipe() {
     touchStart = e.changedTouches[0].screenX;
   });
 
-<<<<<<< HEAD
   document.addEventListener("touchend", (e) => {
     touchEnd = e.changedTouches[0].screenX;
     checkDirection();
   });
   return (
-    <div>
-      <Navbar color="red"></Navbar>
+    <div className="swipe-body">
+      <Navbar color="blue"></Navbar>
+      <div className="left-arrow"></div>
+      <div className="image"></div>
+      <div className="right-arrow"></div>
     </div>
   );
 }
-=======
-    document.addEventListener('touchend', e => {
-        touchEnd = e.changedTouches[0].screenX
-    checkDirection()
-    })
-    return (
-        <div className="swipe-body">
-            <Navbar color="blue"></Navbar>
-            <div className="left-arrow"></div>
-            <div className="image"></div>
-            <div className="right-arrow"></div>
-
-        </div>
-    );
-}
->>>>>>> 1f4bdf52124dc86fb9b0e45e5b0ece254fc66207
